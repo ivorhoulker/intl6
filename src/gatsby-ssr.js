0ce4fff84a72ef195e2bbb2a105fd8780aab67b7
Ivor <ivor@Michelles-MBP.home>
2020-04-09 20:27:17 +0100

    added ltr

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -29,7 +29,7 @@ export const wrapPageElement = ({ element, props }, pluginOptions) => {
   };
   return (
     <React.Fragment>
-      <Helmet htmlAttributes={{ lang }}>
+      <Helmet htmlAttributes={{ lang, dir: 'ltr' }}>
         <meta property="og:locale" content={lang} />
         <link rel="canonical" href={`${siteUrl}/${lang}${originalPath}`} />
         <link rel="alternate" href={`${siteUrl}${originalPath}`} hrefLang="x-default" />
